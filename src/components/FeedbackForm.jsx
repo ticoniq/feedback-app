3dae7cabb5f4fbc179378893797d1cfff9c92ed2
ticoniq_ <okolietochi@gmail>
2023-04-16 04:05:33 +0200

    Fsorm real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,21 @@ import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
     setText(e.target.value)
   }
   
@@ -15,8 +28,10 @@ function FeedbackForm() {
         <h2>How would rate your service with us</h2>
         <div className='input-group'>
           <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text}/>
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
