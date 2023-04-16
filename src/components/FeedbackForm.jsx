f63d6e9ee904ae8b0a50d79ff2c8dc953b61e233
ticoniq_ <okolietochi@gmail>
2023-04-16 02:39:35 +0200

    Custom button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
@@ -14,7 +15,7 @@ function FeedbackForm() {
         <h2>How would rate your service with us</h2>
         <div className='input-group'>
           <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text}/>
-          <button type="submit">Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
