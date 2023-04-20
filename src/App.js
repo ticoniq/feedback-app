33e55d07d3f85ee5133cb11f4beb79005ce7a873
Ticoniq <okolietochi@gmail.com>
2023-04-20 09:26:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
-import AboutIconLink from './components/AboutIconLink'
-import AboutPage from './pages/AboutPage'
-import { FeedbackProvider } from './context/FeedbackContext'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutIconLink from "./components/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
   return (
     <FeedbackProvider>
       <Router>
         <Header />
-        <div className='container'>
+        <div className="container">
           <Routes>
             <Route
-              path='/'
+              path="/"
               element={
                 <>
                   <FeedbackForm />
@@ -25,14 +25,14 @@ function App() {
               }
             ></Route>
 
-            <Route path='/about' element={<AboutPage />} />
+            <Route path="/about" element={<AboutPage />} />
           </Routes>
 
           <AboutIconLink />
         </div>
       </Router>
     </FeedbackProvider>
-  )
+  );
 }
 
-export default App
+export default App;
